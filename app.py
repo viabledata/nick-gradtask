7acc3ab8b4e0da687bdf5b8f937989794c4133ed
nickmorris <nick.morris@viabledata.co.uk>
2024-05-01 20:46:58 +0100

    move create_app to __init__.py

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-
-database = SQLAlchemy()
-
-
-def create_app(config_class=Config) -> Flask:
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-    database.init_app(app)
-
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-
-    from blueprints.excel import excel
-    app.register_blueprint(excel)
-
-    from blueprints.users import users
-    app.register_blueprint(users)
-
-    return app
-
+from app import create_app
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
